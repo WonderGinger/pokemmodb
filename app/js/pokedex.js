dbdd95e45ac5cca02445a6f8f8e403fbd1c2c329
Wonder Ginger <brandt.willems@gmail.com>
2018-07-10 19:07:51 -0700

    The table works and can be sorted and searched by name or type. Next is adding pages for each entry with moves

--- app/js/pokedex.js
+++ app/js/pokedex.js
@@ -1,13 +1,56 @@
 const $ = require('jquery')
-var Promise = require('bluebird')
-const fs = Promise.promisifyAll(require('fs'))
+require('bootstrap')
+var dt = require('datatables.net-bs4')(window, $)
+var pokedexObj = require('./app/pokedex/pokemmo_full.json')
 
-function loadDex () {
-  return fs.readFileAsync('./app/pokedex/pokemmo_min.json')
-}
+$(document).ready(function () {
+  populateTable(pokedexObj)
+})
 
-window.onload = () => {
-  loadDex().then((pokedex) => {
-    console.log(pokedex)
+function populateTable (pokedex) {
+  console.log(pokedex)
+  var trHTML = ''
+
+  $.each(pokedex, function (i, item) {
+    let total = 0
+    for (j = 0; j <= 5; j++) {
+      total += pokedex[i].stats[j].base_stat
+    }
+    let name = pokedex[i].species.name
+    name = capitalize(name)
+    let types = ''
+    for (let type of pokedex[i].types) {
+      types += `<span class="type ${type.type.name}">${type.type.name}</span></br>`
+    }
+    trHTML += '<tr><td class="align-middle">' + pokedex[i].id +
+      '</td><td class="align-middle">' + name + '</td>' +
+      '</td><td class="align-middle">' + types + '</td>' +
+      '</td><td class="align-middle">' + total + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[5].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[4].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[3].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[2].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[1].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[0].base_stat + '</td></tr>'
+  })
+  $('#table').append(`<tbody>${trHTML}</tbody>`)
+  $('#table').DataTable({
+    'dom': 'lfrtip',
+    'paging': false,
+    'columnDefs': [
+      { 'orderSequence': ['desc'], 'targets': [ 3, 4, 5, 6, 7, 8, 9 ] }
+    ],
+    'language': {
+      search: '_INPUT_',
+      searchPlaceholder: 'Search pokedex'
+    }
   })
 }
+
+function capitalize (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+// pokedex.sort((a, b) => {
+//   return a.id - b.id
+// })
--- app/js/scrape.js
+++ app/js/scrape.js
@@ -1,66 +1,66 @@
-const $ = require('jquery')
-var Pokedex = require('pokedex-promise-v2')
-var Promise = require('bluebird')
-const fs = Promise.promisifyAll(require('fs'))
-var pokedex150to237 = []
-var jsonEntries
-let options = {
-  protocol: 'https',
-  versionPath: '/api/v2/',
-  timeout: 600 * 1000
-}
-var P = new Pokedex(options)
+// const $ = require('jquery')
+// var Pokedex = require('pokedex-promise-v2')
+// var Promise = require('bluebird')
+// const fs = Promise.promisifyAll(require('fs'))
+// var pokedex150to237 = []
+// var jsonEntries
+// let options = {
+//   protocol: 'https',
+//   versionPath: '/api/v2/',
+//   timeout: 600 * 1000
+// }
+// var P = new Pokedex(options)
 
-function loadDex () {
-  return fs.readFileAsync('./app/pokedex/pokemmo_min.json')
-}
-function getMon (id) {
-  P.getPokemonByName(id)
-    .then(function (response) {
-      console.log(response)
-      pokedex150to237.push(response)
-      return response
-    })
-    .catch(function (error) {
-      console.log('There was an ERROR: ', error)
-    })
-}
-
-function getDex (entries) {
-  let promises = []
-  let i = 1
-  let end = 75
-  for (let entry of entries.slice(150)) {
-    promises.push(getMon(entry.id))
-    i++
-    if (i > end) return Promise.all(promises)
-  }
-  return Promise.all(promises)
-}
-
-// var doneWriting = false
-// window.onload = () => {
-//   if (doneWriting) return
-//   loadDex().then((data) => {
-//     let pokemmoMin = JSON.parse(data)
-//     console.log(pokemmoMin)
-//     jsonEntries = pokemmoMin
-
-//     console.log('dex length: ', pokemmoMin.length)
-//     // try { fs.openSync('./app/pokedex/pokemmo_full.txt', 'wx') } catch (e) { console.log(e) }
-
-//     getDex(pokemmoMin).then((pokedex) => {
-//       console.log(pokedex)
-//       // pokedex1to75 = pokedex
-//       doneWriting = true
-//     }, (err) => {
-//       throw err
+// function loadDex () {
+//   return fs.readFileAsync('./app/pokedex/pokemmo_min.json')
+// }
+// function getMon (id) {
+//   P.getPokemonByName(id)
+//     .then(function (response) {
+//       console.log(response)
+//       pokedex150to237.push(response)
+//       return response
 //     })
-//   }, (err) => {
-//     throw err
-//   })
+//     .catch(function (error) {
+//       console.log('There was an ERROR: ', error)
+//     })
 // }
 
-function saveDex (filename, dex) {
-  fs.writeFile(`./app/pokedex/${filename}`, JSON.stringify(dex), (err) => { console.log('Failed to save', err) })
-}
+// function getDex (entries) {
+//   let promises = []
+//   let i = 1
+//   let end = 75
+//   for (let entry of entries.slice(150)) {
+//     promises.push(getMon(entry.id))
+//     i++
+//     if (i > end) return Promise.all(promises)
+//   }
+//   return Promise.all(promises)
+// }
+
+// // var doneWriting = false
+// // window.onload = () => {
+// //   if (doneWriting) return
+// //   loadDex().then((data) => {
+// //     let pokemmoMin = JSON.parse(data)
+// //     console.log(pokemmoMin)
+// //     jsonEntries = pokemmoMin
+
+// //     console.log('dex length: ', pokemmoMin.length)
+// //     // try { fs.openSync('./app/pokedex/pokemmo_full.txt', 'wx') } catch (e) { console.log(e) }
+
+// //     getDex(pokemmoMin).then((pokedex) => {
+// //       console.log(pokedex)
+// //       // pokedex1to75 = pokedex
+// //       doneWriting = true
+// //     }, (err) => {
+// //       throw err
+// //     })
+// //   }, (err) => {
+// //     throw err
+// //   })
+// // }
+
+// function saveDex (filename, dex) {
+//   fs.writeFile(`./app/pokedex/${filename}`, JSON.stringify(dex), (err) => { console.log('Failed to save', err) })
+// }
