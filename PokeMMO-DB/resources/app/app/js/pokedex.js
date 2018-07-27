d6bb3e04e7d620f9494d7cf9062bc22d700af794
Wonder Ginger <brandt.willems@gmail.com>
2018-07-27 01:03:17 -0700

    Added type searching. Use for example dragon-type in the search bar.

---
+++ PokeMMO-DB/resources/app/app/js/pokedex.js
@@ -0,0 +1,82 @@
+const $ = require('jquery')
+require('bootstrap')
+var dt = require('datatables.net-bs4')(window, $)
+var pokedexObj = require('./app/pokedex/pokemmo_min.json')
+var pool = require('./app/pokedex/custom_pool.json')
+var table
+
+$(document).ready(function () {
+  // populateTable(pool)
+  populateTable(pokedexObj)
+})
+
+function populateTable (pokedex) {
+  console.log(pokedex)
+  var trHTML = ''
+  $.each(pokedex, function (i, item) {
+    let total = 0
+    for (j = 0; j <= 5; j++) {
+      total += pokedex[i].stats[j].base_stat
+    }
+    let name = pokedex[i].species.name
+    name = capitalize(name)
+    let types = ''
+    for (let type of pokedex[i].types) {
+      types += `<span class="type ${type.type.name}">${type.type.name}</span></br>`
+    }
+    let abilities = ''
+    for (let ability of pokedex[i].abilities) {
+      abilities += `${ability.ability.name} `
+    }
+    let moves = ''
+    for (let move of pokedex[i].moves) {
+      moves += `${move.move.name} `
+    }
+    trHTML += '<tr><td class="align-middle">' + pokedex[i].id +
+      '</td><td class="align-middle">' + `<a href="https://pokemondb.net/pokedex/${name}">${name}</a>` + '</td>' +
+      '</td><td class="align-middle">' + types + '</td>' +
+      // HIDDEN COLUMNS FOR SEARCHING [ 3, 4 ]
+      '</td><td>' + abilities + '</td>' +
+      '</td><td>' + moves + '</td>' +
+
+      '</td><td class="align-middle">' + total + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[5].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[4].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[3].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[2].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[1].base_stat + '</td>' +
+      '</td><td class="align-middle">' + pokedex[i].stats[0].base_stat + '</td>' +
+
+      '</tr>'
+  })
+  $('#table').append(`<tbody>${trHTML}</tbody>`)
+  table = $('#table').DataTable({
+    'dom': '<"top"f>rt<"bottom"f><"clear">',
+    'paging': false,
+    'columnDefs': [
+      { 'orderData': [ 5, 11 ], 'targets': 5 },
+      {
+        'targets': [ 3 ],
+        'visible': false
+      },
+      {
+        'targets': [ 4 ],
+        'visible': false
+      }
+    ],
+    'language': {
+      search: '_INPUT_',
+      searchPlaceholder: 'Search pokedex'
+    }
+  })
+}
+
+function capitalize (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function sortDex (data) {
+  data.sort((a, b) => {
+    return a.id - b.id
+  })
+}
---
+++ PokeMMO-DB/resources/app/app/js/update_dex.js
@@ -0,0 +1,39 @@
+// const fs = require('fs')
+var pokedexFull = require('./app/pokedex/pokemmo_full.json')
+var customNames = require('./app/pokedex/custom_names.json')
+var pokedexReduced
+var customPool = []
+$(document).ready(function () {
+  // pokedexReduced = trimDex(pokedexFull)
+  // fillPool(pokedexReduced)
+  // fs.writeFileSync('./app/pokedex/pokemmo_min.json', JSON.stringify(pokedexReduced))
+  // fs.writeFileSync('./app/pokedex/custom_pool.json', JSON.stringify(customPool))
+})
+
+function trimDex (pokedex) {
+  for (let entry of pokedex) {
+    trimEntry(entry)
+  }
+  return pokedex
+}
+
+function trimEntry (entry) {
+  for (let move of entry.moves) {
+    delete move.version_group_details
+    delete move.move.url
+  }
+}
+
+function fillPool (pokedex) {
+  for (let drop of customNames) {
+    addDrop(drop)
+  }
+}
+
+function addDrop (name) {
+  for (let entry of pokedexFull) {
+    if (entry.name === name) {
+      customPool.push(entry)
+    }
+  }
+}
